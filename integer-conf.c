72e21f8dff6b127282203bd570d9fee3bc3071c1
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-06-08 12:00:48 +0000

    integer-conf from common source.
    git-svn-id: file:///source/coreland/integer/trunk@2417 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- integer-conf.c
+++ integer-conf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "ctxt.h"
 
-const char progname[] = "integer-conf";
-
-int str_diff(register const char *, register const char *);
-long str_rchr(register const char *, register int);
+static unsigned int flag;
+static int str_diff(register const char *, register const char *);
+static long str_rchr(register const char *, register int);
 
 #define FLAG_INCDIR  0x0001
 #define FLAG_DLIBDIR 0x0002
@@ -16,94 +16,123 @@ long str_rchr(register const char *, register int);
 #define FLAG_CFLAGS  0x0080
 #define FLAG_LDFLAGS 0x0100
 
-struct flag { const char *flag; unsigned int val; const char *desc; };
-static const struct flag flags[] = {
-  { "incdir",  FLAG_INCDIR, "print include directory" },
-  { "dlibdir", FLAG_DLIBDIR, "print dynamic library directory" },
-  { "slibdir", FLAG_SLIBDIR, "print static library directory" },
-  { "compile", FLAG_COMPILE, "modify output for use as compiler flags" },
-  { "cflags",  FLAG_CFLAGS,  "output required compiler flags" },
-  { "ldflags", FLAG_LDFLAGS, "output required linker flags" },
-  { "version", FLAG_VERSION, "print library version" },
-  { "help",    FLAG_HELP, "this message" },
-  { "newline", FLAG_NEWLINE, "print trailing newline" },
+/* PROJECT SPECIFIC */
+
+const char progname[] = "integer-conf";
+
+void flag_incdir(void)
+{
+  unsigned long pos;
+  if (flag & FLAG_COMPILE) {
+    pos = str_rchr(ctxt_incdir, '/');
+    if (pos)
+      ctxt_incdir[str_rchr(ctxt_incdir, '/')] = 0;
+    printf("-I%s ", ctxt_incdir);
+  } else {
+    printf("%s ", ctxt_incdir);
+  }
+}
+void flag_cflags(void)
+{
+
+}
+void flag_dlibdir(void)
+{
+  if (flag & FLAG_COMPILE) printf("-L");
+  printf("%s ", ctxt_dlibdir);
+}
+void flag_slibdir(void)
+{
+  if (flag & FLAG_COMPILE) printf("-L");
+  printf("%s ", ctxt_slibdir);
+}
+void flag_ldflags(void)
+{
+
+}
+
+/* PROJECT SPECIFIC END */
+
+void flag_incdir(void);
+void flag_dlibdir(void);
+void flag_slibdir(void);
+void flag_cflags(void);
+void flag_ldflags(void);
+
+static void flag_version(void);
+static void flag_help(void);
+static void flag_newline(void);
+
+static const struct {
+  const char *flag;
+  void (*func)(void);
+  unsigned int val;
+  const char *desc;
+} flags[] = {
+  { "help",    flag_help,    FLAG_HELP,    "this message" },
+  { "compile", 0,            FLAG_COMPILE, "modify output for use as compiler flags" },
+  { "incdir",  flag_incdir,  FLAG_INCDIR,  "print include directory" },
+  { "cflags",  flag_cflags,  FLAG_CFLAGS,  "output required compiler flags" },
+  { "dlibdir", flag_dlibdir, FLAG_DLIBDIR, "print dynamic library directory" },
+  { "slibdir", flag_slibdir, FLAG_SLIBDIR, "print static library directory" },
+  { "ldflags", flag_ldflags, FLAG_LDFLAGS, "output required linker flags" },
+  { "version", flag_version, FLAG_VERSION, "print library version" },
+  { "newline", flag_newline, FLAG_NEWLINE, "print trailing newline" },
 };
 
-void usage()
-{
-  printf("%s: [help] ops ...\n", progname);
-}
-void help()
+static void usage(void) { printf("%s: [help] ops ...\n", progname); }
+static void help(void)
 {
   unsigned int ind;
   usage();
   printf("possible operators:\n");
-  for (ind = 0; ind < sizeof(flags) / sizeof(struct flag); ++ind)
+  for (ind = 0; ind < sizeof(flags) / sizeof(flags[0]); ++ind)
     printf("%s - %s\n", flags[ind].flag, flags[ind].desc);
+  exit(0);
 }
-
-int main(int argc, char *argv[])
+static void parse_flags(int argc, char *argv[])
 {
-  unsigned int flag;
   unsigned int ind;
   unsigned int jnd;
 
   --argc;
   ++argv;
 
-  if (!argc) { usage(); return 111; }
+  if (!argc) { usage(); exit(111); }
 
   flag = 0;
   for (ind = 0; ind < argc; ++ind) {
-    for (jnd = 0; jnd < sizeof(flags) / sizeof(struct flag); ++jnd) {
+    for (jnd = 0; jnd < sizeof(flags) / sizeof(flags[0]); ++jnd) {
       if (str_diff(argv[ind], flags[jnd].flag) == 0) {
         flag |= flags[jnd].val;
         break;
       }
     }
   }
-  if (flag & FLAG_HELP) { help(); return 0; }
-  if (flag & FLAG_VERSION) printf("%s ", ctxt_version);
-  if (flag & FLAG_INCDIR) {
-    if (flag & FLAG_COMPILE) {
-      ctxt_incdir[str_rchr(ctxt_incdir, '/')] = 0;
-      printf("-I%s ", ctxt_incdir);
-    } else {
-      printf("%s ", ctxt_incdir);
-    }
+}
+static void call_flags(void)
+{
+  unsigned int ind;
+  for (ind = 0; ind < sizeof(flags) / sizeof(flags[0]); ++ind) {
+    if (flag & flags[ind].val)
+      if (flags[ind].func)
+        flags[ind].func();
   }
-  if (flag & FLAG_CFLAGS) {
-#ifdef SYS_HAVE_CPU_EXT_SSE
-    if (ctxt_flags_sse_len > 1) printf("%s ", ctxt_flags_sse);
-#endif
-#ifdef SYS_HAVE_CPU_EXT_SSE2
-    if (ctxt_flags_sse2_len > 1) printf("%s ", ctxt_flags_sse2);
-#endif
-#ifdef SYS_HAVE_CPU_EXT_SSE3
-    if (ctxt_flags_sse3_len > 1) printf("%s ", ctxt_flags_sse3);
-#endif
-#ifdef SYS_HAVE_CPU_EXT_ALTIVEC
-    if (ctxt_flags_altivec_len > 1) printf("%s ", ctxt_flags_altivec);
-#endif
-    if (ctxt_flags_math_len > 1) printf("%s ", ctxt_flags_math);
-  }
-  if (flag & FLAG_DLIBDIR) {
-    if (flag & FLAG_COMPILE) printf("-L");
-    printf("%s ", ctxt_dlibdir);
-  }
-  if (flag & FLAG_SLIBDIR) {
-    if (flag & FLAG_COMPILE) printf("-L");
-    printf("%s ", ctxt_slibdir);
-  }
-  if (flag & FLAG_LDFLAGS) {
-    if (ctxt_libs_math_len > 2) printf("%s ", ctxt_libs_math);
-  }
-  if (flag & FLAG_NEWLINE) printf("\n");
+}
+static void flag_version(void) { printf("%s ", ctxt_version); }
+static void flag_newline(void) { printf("\n"); }
+static void flag_help(void) { help(); }
+int main(int argc, char *argv[])
+{
+  parse_flags(argc, argv);
+  call_flags();
+
+  if (fflush(0) != 0) return 112;
   return 0;
 }
 
 /* portability functions */
-int str_diff(register const char *s, register const char *t)
+static int str_diff(register const char *s, register const char *t)
 {
   register char u;
   for (;;) {
@@ -115,7 +144,7 @@ int str_diff(register const char *s, register const char *t)
   return ((int)(unsigned int)(unsigned char) u) - 
          ((int)(unsigned int)(unsigned char) *t);
 }
-long str_rchr(register const char *s, register int c)
+static long str_rchr(register const char *s, register int c)
 {
   register const char *t;
   register const char *u;
